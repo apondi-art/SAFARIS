c8da879b89572d88cbf37dfd64e019e2649a5082
quochieng <aiyanarich64@gmail.com>
2024-07-30 22:14:19 +0300

    fea: html

--- handlers/getmethod.go
+++ handlers/getmethod.go
@@ -47,23 +47,23 @@ func DriverHandle(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		return
 	}
-	name := r.Form.Get("full-name")
-	number := r.Form.Get("phone")
-	id := r.Form.Get("id-number")
-	plate := r.Form.Get("vehicle-plate")
-	Drivers.AddDriver(name, id, plate, number)
+	// name := r.Form.Get("full-name")
+	// number := r.Form.Get("phone")
+	// id := r.Form.Get("id-number")
+	// plate := r.Form.Get("vehicle-plate")
+	// Drivers.AddDriver(name, id, plate, number)
 
-	var driver string
-	for i, block := range Drivers.Drivers {
-		driver = fmt.Sprintf("Block %d:\n", i+1)
-		driver += fmt.Sprintf("  Hash: %s\n", block.Hash)
-		driver += fmt.Sprintf("  Previous Hash: %s\n", block.Name)
-		driver += fmt.Sprintf("    Driver ID: %s\n", block.ID)
-		driver += fmt.Sprintf("    User: %+v\n", block.VehicleReg)
-		driver += fmt.Sprintf("    User: %+v\n", block.TimeStamp)
-	}
-	data := Result{driver}
-	err := t.ExecuteTemplate(w, "driver.html", data)
+	// var driver string
+	// for i, block := range Drivers.Drivers {
+	// 	driver = fmt.Sprintf("Block %d:\n", i+1)
+	// 	driver += fmt.Sprintf("  Hash: %s\n", block.Hash)
+	// 	driver += fmt.Sprintf("  Previous Hash: %s\n", block.Name)
+	// 	driver += fmt.Sprintf("    Driver ID: %s\n", block.ID)
+	// 	driver += fmt.Sprintf("    User: %+v\n", block.VehicleReg)
+	// 	driver += fmt.Sprintf("    User: %+v\n", block.TimeStamp)
+	// }
+	// data := Result{driver}
+	err := t.ExecuteTemplate(w, "driver.html", nil)
 	if err != nil {
 		http.Error(w, "internalServer Error", http.StatusInternalServerError)
 	}
