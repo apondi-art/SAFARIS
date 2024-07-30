b7a3fd9e7e159e100197dd5db7d1e008a376027d
quochieng <aiyanarich64@gmail.com>
2024-07-30 21:00:11 +0300

    feat:added  features to handle templates html

--- handlers/getmethod.go
+++ handlers/getmethod.go
@@ -4,16 +4,47 @@ import (
 	"net/http"
 	"text/template"
 )
-
+var t = template.Must(template.ParseGlob("./templates/*.html"))
+	
 func HomeHandle(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "method Not Allowed", http.StatusMethodNotAllowed)
 	}
-	t, err := template.ParseFiles("templates/index.html")
+	
+	err := t.ExecuteTemplate(w, "index.html", nil)
 	if err != nil {
-		http.Error(w, "Error while parsing The file", http.StatusBadRequest)
+		http.Error(w, "internalServer Error", http.StatusInternalServerError)
 	}
-	err = t.Execute(w, nil)
+}
+
+func JoinusHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "method Not Allowed", http.StatusMethodNotAllowed)
+	}
+	
+	err := t.ExecuteTemplate(w, "joinus.html", nil)
+	if err != nil {
+		http.Error(w, "internalServer Error", http.StatusInternalServerError)
+	}
+}
+
+func DriverHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "method Not Allowed", http.StatusMethodNotAllowed)
+	}
+	
+	err := t.ExecuteTemplate(w, "driver.html", nil)
+	if err != nil {
+		http.Error(w, "internalServer Error", http.StatusInternalServerError)
+	}
+}
+
+func UserHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "method Not Allowed", http.StatusMethodNotAllowed)
+	}
+	
+	err := t.ExecuteTemplate(w, "user.html", nil)
 	if err != nil {
 		http.Error(w, "internalServer Error", http.StatusInternalServerError)
 	}
